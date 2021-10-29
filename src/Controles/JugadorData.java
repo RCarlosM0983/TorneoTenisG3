e30d3f1eea895acc585679cdadc210775a54afac
RCarlosM0983 <90623597+RCarlosM0983@users.noreply.github.com>
2021-10-29 19:41:04 -0300

    cambios

--- src/Controles/JugadorData.java
+++ src/Controles/JugadorData.java
@@ -16,8 +16,7 @@ public class JugadorData {
     
     }
    public void guardarJugador(Jugador j){
-    String sql = "INSERT INTO jugador(nombreApellido, dni, FechaNac, altura, peso, estilo, manoHabil, "
-            + " activo) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )";
+    String sql = "INSERT INTO jugador(nombreApellido, dni, FechaNac, altura, peso, estilo, manoHabil, activo) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )";
     
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -44,112 +43,37 @@ public class JugadorData {
             JOptionPane.showMessageDialog(null, "No se pudo guardar el jugador ");
         }
     }
-
- /* public List<Alumno> obtenerAlumnos(){
-        Alumno a;
-        ArrayList<Alumno> alumnos=new ArrayList<>();        
-        String sql="SELECT * FROM alumno";
-        
+   public void actualizarJugador(Jugador j){
         try {
-            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs= ps.executeQuery();
-        
-            while(rs.next()){
-                a = new Alumno();
-                a.setLegajo(rs.getInt("legajo"));
-                a.setActivo(rs.getBoolean("activo"));
-                a.setNombre(rs.getString("nombre"));
-                a.setApellido(rs.getString("apellido"));
-                a.setFechaNac(rs.getDate("fechaNac").toLocalDate());
-                a.setIdAlumno(rs.getInt("idAlumno"));
-                alumnos.add(a);
-            }
-            
-            ps.close();
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error de conexion.");
-        }
-        return alumnos;
-    }
-   
-  public Alumno buscarAlumno(int idAlumno){
-      Alumno a = null;
-      String sql = "SELECT * FROM alumno WHERE idAlumno = ? ";
-      
-    PreparedStatement ps;
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, idAlumno);
-            ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
-                a = new Alumno();
-                
-                a.setIdAlumno(rs.getInt("idAlumno"));
-                a.setApellido(rs.getString("apellido"));
-                a.setNombre(rs.getString("nombre"));
-                a.setFechaNac(rs.getDate("fechaNac").toLocalDate());
-                a.setLegajo(rs.getInt("legajo"));
-                a.setActivo(rs.getBoolean("activo"));
-                
-            }
-            ps.close();
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error");
-        }
-   
-    return a;
-  }
-  
-  public void borrarAlumno(int idAlumno){
-      String sql = "UPDATE alumno SET activo = false WHERE idAlumno = ?";
-    
-      try {
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setInt(1, idAlumno);
-        
-         ResultSet rs =ps.executeQuery();
-        
-      if(ps.executeUpdate()>0){
-        JOptionPane.showMessageDialog(null, "Alumno borrado");
-      }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al eliminar alumno");
-        }   
-    }
-  
-  public void actualizarAlumno(Alumno a){
-        try {
-            String sql = "UPDATE alumno SET apellido = ?, nombre = ?, fechaNac = ?, legajo = ?, activo = ? WHERE idAlumno = ?";
+            String sql = "UPDATE jugador SET nombreApellido = ?, dni = ?, fechaNac = ?, altura = ?, peso = ?, estilo = ?, manoHabil = ?, activo = ? WHERE idJugador = ?";
             
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             
             
-            ps.setString(1, a.getApellido());
-            ps.setString(2, a.getNombre());
-            //ps.setDate(3, a.getFechaNac().toLocalDate());
-            ps.setDate(3, Date.valueOf(a.getFechaNac()));
-            ps.setInt(4, a.getLegajo());
-            ps.setBoolean(5, a.isActivo());
-            ps.setInt(6, a.getIdAlumno());
+           ps.setString(1, j.getNombreApellido());
+            ps.setInt(2, j.getDni());
+            ps.setDate(3,  Date.valueOf(j.getFechaNac()));
+            ps.setDouble(4, j.getAltura());
+            ps.setDouble(5, j.getPeso());
+            ps.setString(6, j.getEstilo());
+            ps.setString(7, j.getManoHabil());
+            ps.setBoolean(8, j.isActivo());
+            ps.setInt(9, j.getIdJugador());
+            
             
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             
            if(ps.executeUpdate()>0){
-        JOptionPane.showMessageDialog(null, "Alumno actualizado");
+        JOptionPane.showMessageDialog(null, "Jugador actualizado");
         
       }else{
-              JOptionPane.showMessageDialog(null, "El alumno no existe"); 
+              JOptionPane.showMessageDialog(null, "El Jugador no existe"); 
            }
             ps.close();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al actualizar alumno");
+            JOptionPane.showMessageDialog(null, "Error al actualizar Jugador");
         }
-  }*/
+  }
 }
-
 
--- src/torneotenis/TorneoTenis.java
+++ src/torneotenis/TorneoTenis.java
@@ -23,9 +23,10 @@ public class TorneoTenis {
         // TODO code application logic here
         Conexion c = new Conexion();
         JugadorData jd = new JugadorData(c);
-        Jugador j = new Jugador("juan gonz", 35642310, LocalDate.of(1983, 8, 9), 1.8, 78.5, "polvo ladrillo", 
-                "derecho", true);
-        jd.guardarJugador(j);
+        
+        
+        Jugador j1 = new Jugador(24, "Juan Lopez", 23487624, LocalDate.of(1991, 04, 25), 1.8, 78.5, "Defensivo", "derecho", true);
+        jd.actualizarJugador(j1);
     }
     
 }
